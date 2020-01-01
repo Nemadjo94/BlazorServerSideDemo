0d4ae94eddddccebf72c99d367dcf9dd5042acc0
Nemanja Djordjevic <nemadjo94@gmail.com>
2020-01-01 19:44:09 +0100

    Implemented People Page

---
+++ BlazorServer/Models/DisplayPersonModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace BlazorServer.Models
+{
+    public class DisplayPersonModel
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "First Name is too long")]
+        [MinLength(1, ErrorMessage = "First Name is too short")]
+        public string FirstName { get; set; }
+
+        [StringLength(15, ErrorMessage = "Last Name is too long")]
+        [MinLength(1, ErrorMessage = "Last Name is too short")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
